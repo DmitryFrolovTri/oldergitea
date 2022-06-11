332756d9ed82cd8d9a526ac97a0586d38fa20c99
Gitea <gitea@fake.local>
2022-06-11 23:58:18 +0300

    Сообщение о превышении квоты вместо 500-й ошибки

--- models/repo/update.go
+++ models/repo/update.go
@@ -100,6 +100,21 @@ func (err ErrRepoFilesAlreadyExist) Error() string {
 	return fmt.Sprintf("repository files already exist [uname: %s, name: %s]", err.Uname, err.Name)
 }
 
+// ErrКвотаПревышена возвращается при переполнении квоты
+type ErrКвотаПревышена struct {
+	Кто string
+}
+
+// IsErrКвотаПревышена checks if an error is a ErrКвотаПревышена.
+func IsErrКвотаПревышена(err error) bool {
+	_, ok := err.(ErrКвотаПревышена)
+	return ok
+}
+
+func (err ErrКвотаПревышена) Error() string {
+	return "пользователь или деятель - за пределеами квоты"
+}
+
 // CheckCreateRepository check if could created a repository
 func CheckCreateRepository(doer, u *user_model.User, name string, overwriteOrAdopt bool) error {
 	if !doer.CanCreateRepo() {
@@ -118,11 +133,11 @@ func CheckCreateRepository(doer, u *user_model.User, name string, overwriteOrAdo
 	}
 
 	if !doer.ВПределахКвотыЛи() {
-		return fmt.Errorf("пользователь, выполняющий действие, вне пределов квоты")
+		return ErrКвотаПревышена{"пользователь"}
 	}
 
 	if !u.ВПределахКвотыЛи() {
-		return fmt.Errorf("владелец создаваемого репозитория - вне пределов квоты")
+		return ErrКвотаПревышена{"владелец"}
 	}
 
 	repoPath := RepoPath(u.Name, name)
--- routers/web/repo/migrate.go
+++ routers/web/repo/migrate.go
@@ -80,6 +80,8 @@ func handleMigrateError(ctx *context.Context, owner *user_model.User, err error,
 		ctx.RenderWithErr(ctx.Tr("form.visit_rate_limit"), tpl, form)
 	case migrations.IsTwoFactorAuthError(err):
 		ctx.RenderWithErr(ctx.Tr("form.2fa_auth_required"), tpl, form)
+	case repo_model.IsErrКвотаПревышена(err):
+		ctx.RenderWithErr("Квота превышена", tpl, form)
 	case repo_model.IsErrReachLimitOfRepo(err):
 		maxCreationLimit := owner.MaxCreationLimit()
 		msg := ctx.TrN(maxCreationLimit, "repo.form.reach_limit_of_creation_1", "repo.form.reach_limit_of_creation_n", maxCreationLimit)
